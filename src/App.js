411cc6da5929262710886a930f9b13f89653cab7
Alejandro Olmedo <00097017@uca.edu.sv>
2020-03-28 18:20:14 -0600

    Subiendo proyecto kakaso xd

--- src/App.js
+++ src/App.js
@@ -1,26 +1,122 @@
 import React from 'react';
-import logo from './logo.svg';
+import {dia} from './Date';
+import shuffle from 'shuffle-array'
+import axios from 'axios'
+import ReactTypingEffect from 'react-typing-effect';
+import { Button, Form, Input, UncontrolledAlert } from 'reactstrap';
+import './App.css'
+
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+setInterval(()=>{}, 1000)
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = ({
+      dias: "",
+      horas:"",
+      minutos:"",
+      segundos:"",
+      Splashes: [],
+      response: "",
+      mensajeCustom: "",
+      alert: false
+    })
+  }
+  changeHandler = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value,
+    })
+  }
+  submitHandler = event => {
+    event.preventDefault();
+
+    const msg = {
+      msg: this.state.mensajeCustom
+    }
+
+    axios.post('https://controlasistenciacooler.herokuapp.com/covid/create/', msg)
+      .then(response => {
+        if (response.status === 200) {
+          this.setState({ alert: true, mensajeCustom: "" })
+        }
+      })
+      .catch(err => console.log(err))
+
+
+  }
+  tick = () =>{
+    var now = new Date();
+    var horas = 24 - now.getHours();
+    var minutos = 60 - now.getMinutes();
+    var segundos = 60- now.getSeconds();
+    this.setState({
+      horas:horas,
+      minutos:minutos,
+      segundos:segundos
+    })
+  }
+  fixArr = (array) => {
+    const arrayFixed = shuffle(array).map(splash => {
+      return splash.msg
+    })
+    this.setState({ Splashes: arrayFixed })
+  }
+  componentDidMount() {
+    let dias = 112 - dia; //112 ->21 Abril
+    if (dias < 0) dias = 0;
+    this.setState({ dias: dias });
+    let response = "SIIIIIIII";
+    let noResponses = ["Nop", "Nopi", "No aun", "Algun día", "Ni de cerca ajjaja", "No", "Nouu", "Intenta mañana lol", "Siiiiii, Wait.. No :((", "Ya se me acabaron las ideas pero todavia no jajajaja"];
+    if (dias !== 0) response = noResponses[Math.floor(Math.random() * noResponses.length)];
+
+    axios.get('https://controlasistenciacooler.herokuapp.com/covid/')
+      .then(splahes => { this.fixArr(splahes.data) })
+      .catch(err => console.log(err))
+
+    this.setState({
+      dias: dias,
+      response: response
+    })
+
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+  
+
+  render() {
+
+    return (
+      <div className="App" >
+        
+        {this.state.alert && <UncontrolledAlert color="info">
+      Desde ahora tu mensaje sera visible por el MUNDO (osea los que se metan a la pagina xD)
+    </UncontrolledAlert>}
+        <h1 className="title">{this.state.response}</h1>
+        <div className="marginado">
+          <p>Dias {this.state.dias}</p>
+          <p >Horas {this.state.horas}</p>
+          <p >Minutos  {this.state.minutos}</p>
+          <p >Segundos  {this.state.segundos}</p>
+        </div>
+        <div className="splashSection">
+          <Form >
+            <div style={{marginBottom:"1em"}}>
+              <Input size={45} className='transparentInput' type="text" name="mensajeCustom" id="mensajeCustom" placeholder="Pongan tusa" onChange={this.changeHandler} value={this.state.mensajeCustom} />
+            </div>
+            <div>
+              <Button color="success" onClick={this.submitHandler}>This should do something <span role="img" aria-label="thinking">🤔</span></Button>
+            </div>
+          </Form>
+          <ReactTypingEffect className='Mine' text={this.state.Splashes} />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
